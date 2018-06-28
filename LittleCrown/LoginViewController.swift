e626752a4dacd9ca05b56ca3e9c3ecf4c16c17db
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2018-06-28 15:11:10 +0200

    Create LittleCrown xcode project

---
+++ LittleCrown/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  LittleCrown
+//
+//  Created by Grzegorz Przybyła on 28/06/2018.
+//  Copyright © 2018 Grzegorz Przybyła. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ LittleCrown/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LittleCrown
+//
+//  Created by Grzegorz Przybyła on 28/06/2018.
+//  Copyright © 2018 Grzegorz Przybyła. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
